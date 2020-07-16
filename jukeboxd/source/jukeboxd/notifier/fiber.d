e4700ff28551d546886ff1c5022f97398749e49d
fence <fence@desu-mail.moe>
2020-07-16 18:08:06 +0200

    ぬるぽ

--- jukeboxd/source/jukeboxd/notifier/fiber.d
+++ jukeboxd/source/jukeboxd/notifier/fiber.d
@@ -4,23 +4,32 @@ import core.thread : Fiber, Thread, dur;
 import vibe.data.bson : Bson, serializeToBson;
 import jukeboxd.protocol;
 import jukeboxd.player;
+import jukeboxd.modules;
 import jukeboxd.notifier.manager;
 
-// NOTE: this is just an experiemnt
 class PlaybackChangeFiber : Fiber {
     private NotifierManager notifier;
     private Player player;
+    private int retries;
 
     this(Player player, NotifierManager notifier) {
         this.player = player;
         this.notifier = notifier;
+        this.retries = 0;
         super(&run);
     }
 
     void run() {
         // wait a bit because we might be playing a network stream;
-        Thread.sleep(dur!("seconds")(1));
-        auto info = player.getPlayingModule().getPlaybackInfo();
-        notifier.notify(MessageKind.PLAYBACK, serializeToBson(info));
+        Thread.sleep(dur!("seconds")(10));
+        PlaybackModule mod = player.getPlayingModule();
+
+        if (mod !is null) {
+            notifier.notify(MessageKind.PLAYBACK, serializeToBson(mod.getPlaybackInfo()));
+        } else {
+            if (retries > 10) {
+                this.run();
+            }
+        }
     }
 }
